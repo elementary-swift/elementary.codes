03b31652eb98163e83dac11ce2ca04aede0f1461
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2026-01-15 08:28:40 +0100

    added sponsorkit CI (#1)

---
+++ .sponsorkit/sponsorkit.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig, tierPresets } from "sponsorkit";
+
+export default defineConfig({
+  github: {
+    login: "sliemeobn",
+    type: "user",
+  },
+  opencollective: {
+    slug: "elementaryui",
+  },
+  outputDir: "../public/sponsors",
+  tiers: [
+    {
+      title: "Sponsors",
+      monthlyDollars: 200,
+      preset: tierPresets.large,
+    },
+    {
+      title: "Pioneers",
+      monthlyDollars: 50,
+      preset: tierPresets.medium,
+    },
+    {
+      title: "Backers",
+      monthlyDollars: 30,
+      preset: tierPresets.small,
+    },
+    {
+      preset: tierPresets.xs,
+      padding: { top: 0 },
+    },
+  ],
+  formats: ["svg"],
+  renderer: "tiers",
+  renders: [
+    {
+      name: "sponsors-400",
+      width: 400,
+    },
+    {
+      name: "sponsors-600",
+      width: 600,
+    },
+    {
+      name: "sponsors-800",
+      width: 800,
+    },
+  ],
+});
